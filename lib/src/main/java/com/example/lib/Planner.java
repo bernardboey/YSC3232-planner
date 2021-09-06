3c498a12f5a47eaed633fd73ffc0f8ef6ac34520
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-09-06 20:53:16 +0800

    Implement part of Planner

--- lib/src/main/java/com/example/lib/Planner.java
+++ lib/src/main/java/com/example/lib/Planner.java
@@ -1,26 +1,40 @@
 package com.example.lib;
 
+import java.util.HashMap;
+
 public class Planner implements IPlanner {
+    final HashMap<String, IBoard> boards;
+    final HashMap<String, IProject> projects;
+
+    public Planner() {
+        this.boards = new HashMap<>();
+        this.projects = new HashMap<>();
+    }
+
     @Override
     public void addBoard(IBoard b) throws AlreadyExistsException {
-        // TODO
+        if (boards.containsKey(b.getName())) {
+            throw new AlreadyExistsException();
+        }
+        boards.put(b.getName(), b);
     }
 
     @Override
     public void addProject(IProject p) throws AlreadyExistsException {
-        // TODO
+        if (boards.containsKey(p.getName())) {
+            throw new AlreadyExistsException();
+        }
+        boards.put(p.getName(), p);
     }
 
     @Override
     public Iterable<IBoard> getBoards() {
-        // TODO
-        return null;
+        return boards.values();
     }
 
     @Override
     public Iterable<IProject> getProjects() {
-        // TODO
-        return null;
+        return projects.values();
     }
 
     @Override
