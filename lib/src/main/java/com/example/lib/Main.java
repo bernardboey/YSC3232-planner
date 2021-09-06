1af395fd39b48cc4f44d689b17ecca5a570b202b
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-09-06 21:07:07 +0800

    Implement main method

--- lib/src/main/java/com/example/lib/Main.java
+++ lib/src/main/java/com/example/lib/Main.java
@@ -1,7 +1,25 @@
 package com.example.lib;
 
+import java.time.Duration;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AlreadyExistsException {
         Planner planner = new Planner();
+        Project project = new Project("YSC3232");
+        Task T1 = new Task("T1", "Task 1", Duration.ofHours(1));
+        Task T2 = new Task("T2", "Task 2", Duration.ofHours(1));
+        Task T3 = new Task("T3", "Task 3", Duration.ofHours(1));
+        project.addTask(T1);
+        project.addTask(T2);
+        project.addTask(T3);
+        Board board = new Board("YSC3232");
+        Section todoSection = new Section("TODO");
+        Section doneSection = new Section("Done");
+        board.addSection(todoSection);
+        board.addSection(doneSection);
+        todoSection.addTask(T1);
+        todoSection.addTask(T2);
+        doneSection.addTask(T3);
+        planner.writeXMLData();
     }
 }
