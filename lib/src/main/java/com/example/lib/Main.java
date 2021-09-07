00fc84984b1d171ed6044b5f313de9bab4b649d0
Bernard Boey <bernard@u.yale-nus.edu.sg>
2021-09-07 23:19:07 +0800

    Implement readXMLData method

--- lib/src/main/java/com/example/lib/Main.java
+++ lib/src/main/java/com/example/lib/Main.java
@@ -5,26 +5,40 @@ import java.time.Duration;
 public class Main {
     public static void main(String[] args) throws AlreadyExistsException {
         Planner planner = new Planner();
+
         Project project = new Project("YSC3232");
         planner.addProject(project);
+
+        Board board = new Board("YSC3232");
+        planner.addBoard(board);
+
+        Section todoSection = new Section("TODO");
+        Section doneSection = new Section("Done");
+        board.addSection(todoSection);
+        board.addSection(doneSection);
+
         Task T1 = new Task("T1", "Task 1", Duration.ofHours(1));
         Task T2 = new Task("T2", "Task 2", Duration.ofHours(1));
         Task T3 = new Task("T3", "Task 3", Duration.ofHours(1));
         project.addTask(T1);
         project.addTask(T2);
         project.addTask(T3);
-        Board board = new Board("YSC3232");
-        planner.addBoard(board);
-        Section todoSection = new Section("TODO");
-        Section doneSection = new Section("Done");
-        board.addSection(todoSection);
-        board.addSection(doneSection);
         todoSection.addTask(T1);
         todoSection.addTask(T2);
         doneSection.addTask(T3);
+
         String xmlData = planner.writeXMLData();
         System.out.println(xmlData);
-        // Planner newPlanner = new Planner();
-        // newPlanner.readXMLData(xmlData);
+
+        Planner newPlanner = new Planner();
+        newPlanner.readXMLData(xmlData);
+        String newXmlData = newPlanner.writeXMLData();
+        System.out.println(newXmlData);
+
+        if (!xmlData.equals(newXmlData)) {
+            System.out.println("XML data not equal");
+        } else {
+            System.out.println("XML data equal");
+        }
     }
 }
--- lib/src/main/java/com/example/lib/Planner.java
+++ lib/src/main/java/com/example/lib/Planner.java
@@ -1,13 +1,12 @@
 package com.example.lib;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.io.StringReader;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -67,6 +66,27 @@ public class Planner implements IPlanner {
         return "<planner>\n" + boardsXml + projectsXml + "</planner>";
     }
 
+    public ITask readTaskXML(Element taskElement) {
+        String taskName = taskElement.getAttribute("name");
+        String taskDescription = taskElement.getAttribute("description");
+        String taskDurationString = taskElement.getAttribute("expected-duration");
+        Duration taskDuration = Duration.parse(taskDurationString);
+        ITask task = new Task(taskName, taskDescription, taskDuration);
+
+        NodeList taskNodes = taskElement.getElementsByTagName("task");
+        for (int i = 0; i < taskNodes.getLength(); i++) {
+            Element subTaskElement = (Element) taskNodes.item(i);
+            ITask subTask = readTaskXML(subTaskElement);
+            try {
+                task.addSubTask(subTask);
+            } catch (AlreadyExistsException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return task;
+    }
+
     @Override
     public void readXMLData(String data) {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -89,25 +109,59 @@ public class Planner implements IPlanner {
 
         NodeList boardNodes = root.getElementsByTagName("board");
         for (int i = 0; i < boardNodes.getLength(); i++) {
-            Element boardNode = (Element) boardNodes.item(i);
-            String boardName = boardNode.getAttribute("name");
+            Element boardElement = (Element) boardNodes.item(i);
+            String boardName = boardElement.getAttribute("name");
             Board board = new Board(boardName);
             try {
                 addBoard(board);
             } catch (AlreadyExistsException e) {
                 e.printStackTrace();
             }
+
+            NodeList sectionNodes = boardElement.getElementsByTagName("section");
+            for (int j = 0; j < sectionNodes.getLength(); j++) {
+                Element sectionElement = (Element) sectionNodes.item(j);
+                String sectionName = sectionElement.getAttribute("name");
+                Section section = new Section(sectionName);
+                try {
+                    board.addSection(section);
+                } catch (AlreadyExistsException e) {
+                    e.printStackTrace();
+                }
+
+                NodeList taskNodes = sectionElement.getElementsByTagName("task");
+                for (int k = 0; k < taskNodes.getLength(); k++) {
+                    Element taskElement = (Element) taskNodes.item(k);
+                    ITask task = readTaskXML(taskElement);
+                    try {
+                        section.addTask(task);
+                    } catch (AlreadyExistsException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
 
         NodeList projectNodes = root.getElementsByTagName("project");
         for (int i = 0; i < projectNodes.getLength(); i++) {
-            Element projectNode = (Element) projectNodes.item(i);
-            String projectName = projectNode.getAttribute("name");
+            Element projectElement = (Element) projectNodes.item(i);
+            String projectName = projectElement.getAttribute("name");
             Project project = new Project(projectName);
             try {
                 addProject(project);
             } catch (AlreadyExistsException e) {
                 e.printStackTrace();
+            }
+
+            NodeList taskNodes = projectElement.getElementsByTagName("task");
+            for (int k = 0; k < taskNodes.getLength(); k++) {
+                Element taskElement = (Element) taskNodes.item(k);
+                ITask task = readTaskXML(taskElement);
+                try {
+                    project.addTask(task);
+                } catch (AlreadyExistsException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
